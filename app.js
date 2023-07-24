37a2a8d671ebfb66dcaf53a27bbb661cec5e59ee
crazzoid123 <melihov89@gmail.com>
2023-07-24 23:02:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,18 @@ let p = document.createElement("p");
 
 let encodedString = tg.initData;
 document.write(encodedString);
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
 
+btn1.addEventListener('click', function(){
+  if (tg.MainButton.isVisible) {
+    tg.MainButton.hide();
+  }
+  else {
+    tg.MainButton.setText('Like');
+    item = 'like';
+    tg.MainButton.show();
+  }
+});
 
