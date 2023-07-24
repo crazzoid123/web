1478361aafa2c45cd81772d84301e55cba4564e2
crazzoid123 <melihov89@gmail.com>
2023-07-24 22:37:14 +0300

    hkhk

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ let p = document.createElement("p");
 let encodedString = tg.initData;
 decodedString = decodeURIComponent(encodedString);
 document.write(decodedString);
+document.write(tg.initDataUnsafe.query_id);
+document.write(tg.initDataUnsafe.user.first_name);
