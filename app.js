8d154d7e2c85ea04b6beb0bb38ea571848c729cc
crazzoid123 <melihov89@gmail.com>
2023-07-24 22:29:47 +0300

    sdsd

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ tg.expand()
 let test = document.getElementById("zal");
 let p = document.createElement("p");
 
-document.write(tg.initData);
-document.write(tg.initDataUnsafe.user.id);
+let encodedString = tg.initData;
+decodedString = decodeURIComponent(encodedString);
+document.write(decodedString);
