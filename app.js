e9c9cbb00f47109f7476bb2d287e3c41cb91cfbe
crazzoid123 <melihov89@gmail.com>
2023-07-24 19:16:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-
+let tg = window.Telegram.WebApp;
+let test = document.getElementById("zal");
+let p = document.createElement("p");
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
