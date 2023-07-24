c15d67736e039c855305de23d0cef3c1f9945d95
crazzoid123 <melihov89@gmail.com>
2023-07-24 23:20:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let tg = window.Telegram.WebApp;
-tg.expand()
+tg.expand();
 
 let encodedString = tg.initData;
 document.write(encodedString);
@@ -8,20 +8,19 @@ let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
 let btn3 = document.getElementById('btn3');
 
-item = '';
+let item = ''; // Add "let" keyword to properly declare the variable.
 
 btn1.addEventListener('click', function() {
   if (tg.MainButton.isVisible) {
     tg.MainButton.hide();
-  }
-  else {
+  } else {
     tg.MainButton.setText('Like');
     item = 'like';
     tg.MainButton.show();
-    
   }
 });
 
-Telegram.WebApp.onEvent('mainButtonClicked'), function() {
+// Correct the function definition below and remove the extra closing parenthesis after 'mainButtonClicked'
+Telegram.WebApp.onEvent('mainButtonClicked', function() {
   tg.sendData(item);
 });
