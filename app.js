ea2440dfd55a0345834e11d5222059b6c5d6285e
crazzoid123 <melihov89@gmail.com>
2023-07-24 23:41:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,11 @@ tg.expand();
 
 let encodedString = tg.initData;
 document.write(encodedString);
-document.write('lol');
 let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
 let btn3 = document.getElementById('btn3');
-document.write('lol1');
 let item = ''; // Add "let" keyword to properly declare the variable.
-document.write(encodedString);
-
 btn1.addEventListener('click', function() {
-  document.write('lol');
   if (tg.MainButton.isVisible) {
     tg.MainButton.hide();
   } else {
@@ -21,7 +16,6 @@ btn1.addEventListener('click', function() {
     tg.MainButton.show();
   }
 });
-document.write('lol2');
 // Correct the function definition below and remove the extra closing parenthesis after 'mainButtonClicked'
 Telegram.WebApp.onEvent('mainButtonClicked', function() {
   tg.sendData(item);
