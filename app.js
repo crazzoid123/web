1ffa25e1dc5220d388b8a02816382bc8784dde88
crazzoid123 <melihov89@gmail.com>
2023-07-24 23:13:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 let tg = window.Telegram.WebApp;
 tg.expand()
-let test = document.getElementById("zal");
-let p = document.createElement("p");
 
 let encodedString = tg.initData;
 document.write(encodedString);
+
 let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
 let btn3 = document.getElementById('btn3');
+
 item = '';
-btn1.addEventListener('click', function(){
+
+btn1.addEventListener('click', function() {
   if (tg.MainButton.isVisible) {
     tg.MainButton.hide();
   }
