38d4162fc277edcdf3b75b5ef19f45a770749f7a
crazzoid123 <melihov89@gmail.com>
2023-07-25 16:15:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ document.write(encodedString);
 let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
 let btn3 = document.getElementById('btn3');
-let item = ''; // Add "let" keyword to properly declare the variable.
+let item = 'test'; // Add "let" keyword to properly declare the variable.
 
 tg.MainButton.setText('Like');
 tg.MainButton.show();
+
+Telegram.WebApp.onEvent('mainButtonClicked', function() {
+  tg.sendData(item);
+});
